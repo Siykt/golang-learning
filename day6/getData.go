83257d0efac3830afd370d776d2542fde1e921e3
siykt <cnsiykt@163.com>
2024-06-20 13:27:55 +0800

    feat: upd check

--- day6/getData.go
+++ day6/getData.go
@@ -7,49 +7,52 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
-	"strings"
 	"sync"
 
 	"github.com/vbauerster/mpb/v7"
 	"github.com/vbauerster/mpb/v7/decor"
 )
 
-func downloadFile(url string, dest string, p *mpb.Progress) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
+func copyAndCheck(dest string, src io.Reader, job string) error {
 	out, err := os.Create(dest)
 	if err != nil {
 		return err
 	}
 	defer out.Close()
 
-	total := resp.ContentLength
-
-	if total < 1*1024 { // Skip files smaller than 1KB
-		_, err = io.Copy(out, resp.Body) // Still need to download to handle renaming
+	buf := make([]byte, 1*1024*1024)
+	n, err := io.CopyBuffer(out, src, buf)
+	if err != nil {
 		return err
 	}
 
-	parts := strings.Split(url, "/")
-	desiredPart := parts[len(parts)-2] + ".rar"
+	if n < 1*1024*1024 {
+		newPath := filepath.Join("G:/HG/", job+".xml")
+		err = os.Rename(dest, newPath)
+		if err != nil {
+			return err
+		}
+	}
 
-	bar := p.AddBar(total,
-		mpb.PrependDecorators(
-			decor.Name(desiredPart+" ", decor.WC{W: len(desiredPart) + 1, C: decor.DidentRight}),
-			decor.CountersKibiByte("%.2f / %.2f"),
-		),
-		mpb.AppendDecorators(
-			decor.Percentage(decor.WC{W: 5}),
-		),
-	)
+	return nil
+}
+
+func downloadFile(url string, dest string, bar *mpb.Bar, job string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	total := resp.ContentLength
+
+	if total < 1*1024 { // Skip files smaller than 1KB
+		return nil
+	}
 
 	proxyReader := bar.ProxyReader(resp.Body)
 
-	_, err = io.Copy(out, proxyReader)
+	err = copyAndCheck(dest, proxyReader, job)
 	if err != nil {
 		return err
 	}
@@ -58,28 +61,29 @@ func downloadFile(url string, dest string, p *mpb.Progress) error {
 	return nil
 }
 
-func worker(jobs <-chan string, p *mpb.Progress, wg *sync.WaitGroup) {
+func worker(id int, jobs <-chan string, p *mpb.Progress, wg *sync.WaitGroup) {
 	defer wg.Done()
+
+	bar := p.AddBar(0, // Initialize with dummy total
+		mpb.PrependDecorators(
+			decor.Name(fmt.Sprintf("Worker %02d ", id), decor.WC{W: 10, C: decor.DidentRight}),
+			decor.EwmaSpeed(decor.UnitKiB, "%6.2f KiB/s ", 60, decor.WC{W: 15}),
+			decor.CountersKibiByte("%.2f / %.2f", decor.WC{W: 20}),
+		),
+		mpb.AppendDecorators(
+			decor.Percentage(decor.WC{W: 5}),
+		),
+	)
+
 	for job := range jobs {
 		dest := filepath.Join("G:/HG/", job+".rar")
 		url := "https://mifeng.oss-cn-hangzhou.aliyuncs.com/" + job + "/GMAE.rar"
 
-		err := downloadFile(url, dest, p)
+		bar.SetTotal(0, false) // Reset bar for new job
+		bar.SetCurrent(0)      // Reset current progress
+		err := downloadFile(url, dest, bar, job)
 		if err != nil {
 			continue
-		}
-
-		fileInfo, err := os.Stat(dest)
-		if err != nil {
-			continue
-		}
-
-		if fileInfo.Size() < 1*1024*1024 {
-			newPath := filepath.Join("G:/HG/", job+".xml")
-			err := os.Rename(dest, newPath)
-			if err != nil {
-				continue
-			}
 		}
 	}
 }
@@ -93,7 +97,7 @@ func main() {
 
 	for w := 1; w <= numWorkers; w++ {
 		wg.Add(1)
-		go worker(jobs, p, &wg)
+		go worker(w, jobs, p, &wg)
 	}
 
 	go func() {
